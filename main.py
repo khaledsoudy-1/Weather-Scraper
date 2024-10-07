64bbe2a627b5ddf4368c45d577550ec2212fff66
Khaled Soudy <khaledsoudy2012@gmail.com>
2024-10-07 21:49:31 +0300

    Add error handling for HTTP requests.

--- main.py
+++ main.py
@@ -7,17 +7,24 @@ def fetch_weather_page(url):
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36'
     }
     
-    # Make a GET request to the specified URL.
-    response = requests.get(url, headers=headers, timeout=7)
+    try:
+        # Make a GET request to the specified URL.
+        response = requests.get(url, headers=headers, timeout=7)
+        response.raise_for_status()  # Raises an error for non-successful requests
+        return response.content  # Return html page content
     
-    if response.status_code == 200:
-        print(response.content)
-        
-    else:
-        print(f"Something went wrong !!: {response.status_code}")
+    except requests.exceptions.HTTPError as http_err:
+        print(f"HTTP error occurred: {http_err}")
+    except requests.exceptions.Timeout as timeout_err:
+        print(f"Timeout error occurred: {timeout_err}")
+    except requests.exceptions.ConnectionError as connect_err:
+        print(f"Connection error occurred. {connect_err}")
+    except requests.exceptions.RequestException as error:
+        print(f"An error occurred: {error}")
+    return None
 
 
 if __name__ == '__main__':
     page_url = "https://world-weather.info/"
-
+    
     fetch_weather_page(page_url)
